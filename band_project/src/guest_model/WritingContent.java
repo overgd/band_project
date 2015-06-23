3d0de910ce120f85a5d5d955f7a3b9890ac78faa
Administrator <Administrator@MSDN-SPECIAL>
2015-06-23 18:29:29 +0900

    LSI.150623

--- band_project/src/guest_model/WritingContent.java
+++ band_project/src/guest_model/WritingContent.java
@@ -2,6 +2,7 @@ package guest_model;
 
 public class WritingContent {
 	private int writingid;
+	private String bandid;
 	private String content;
 	public int getWritingid() {
 		return writingid;
@@ -9,11 +10,17 @@ public class WritingContent {
 	public void setWritingid(int writingid) {
 		this.writingid = writingid;
 	}
+	public String getBandid() {
+		return bandid;
+	}
+	public void setBandid(String bandid) {
+		this.bandid = bandid;
+	}
 	public String getContent() {
 		return content;
 	}
 	public void setContent(String content) {
 		this.content = content;
 	}
-	
+
 }
